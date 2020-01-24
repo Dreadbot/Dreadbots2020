b75e5c3f9a87b646e8cb3dd845f57312fa9dacbf
JohnCollin <collin.g.johnson@gmail.com>
2020-01-24 18:10:55 -0500

    Fix quick formatting error

--- robot/src/main/include/RobotUtilities.h
+++ robot/src/main/include/RobotUtilities.h
@@ -7,9 +7,10 @@
 
 #pragma once
 
-enum JoystickInputs { 
-    x_axis = 0, y_axis = 1, z_axis = 2, w_axis = 3,
-    x_button = 1, a_button = 2, b_button = 3, y_button = 4,
-    left_bumper = 5, right_bumper = 6, left_trigger = 7, right_trigger = 8,
-    back_button = 9, start_button = 10, l_trigger = 11, r_trigger = 12
+enum JoystickInputs 
+{ 
+  x_axis = 0, y_axis = 1, z_axis = 2, w_axis = 3,
+  x_button = 1, a_button = 2, b_button = 3, y_button = 4,
+  left_bumper = 5, right_bumper = 6, left_trigger = 7, right_trigger = 8,
+  back_button = 9, start_button = 10, l_trigger = 11, r_trigger = 12
 };
