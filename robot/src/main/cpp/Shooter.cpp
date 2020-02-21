67da4a3cf89a2b830ccf4ea73e740426cfe04462
JohnCollin <collin.g.johnson@gmail.com>
2020-02-21 11:18:07 -0500

    Shooter.cpp - Add documentation & comments for new code

--- robot/src/main/cpp/Shooter.cpp
+++ robot/src/main/cpp/Shooter.cpp
@@ -1,75 +1,78 @@
 #include <Shooter.h>
+
 Shooter::Shooter(int shooter_motor_id, int aim_motor_id){
-    m_shooterMotor = new rev::CANSparkMax(shooter_motor_id, rev::CANSparkMax::MotorType::kBrushless);
-    m_shooterPid = new rev::CANPIDController(m_shooterMotor->GetPIDController());
-    //Needs to be calibrated
-    m_shooterPid->SetP(6e-5);
-    m_shooterPid->SetI(1e-6);
-    m_shooterPid->SetD(0.3);
-    m_shooterPid->SetIZone(0);  
-    m_shooterPid->SetFF(0.000015);
-    m_shooterPid->SetOutputRange(-1.0, 1.0);
-    speed = 0.0; 
+  shooting_motor = new rev::CANSparkMax(shooter_motor_id, rev::CANSparkMax::MotorType::kBrushless);
+  shooting_motor_pid_controller = new rev::CANPIDController(shooting_motor->GetPIDController());
+  //Needs to be calibrated
+  shooting_motor_pid_controller->SetP(6e-5);
+  shooting_motor_pid_controller->SetI(1e-6);
+  shooting_motor_pid_controller->SetD(0.3);
+  shooting_motor_pid_controller->SetIZone(0);  
+  shooting_motor_pid_controller->SetFF(0.000015);
+  shooting_motor_pid_controller->SetOutputRange(-1.0, 1.0);
+  speed = 0.0; 
 
 
-    m_aimMotor = new rev::CANSparkMax(aim_motor_id, rev::CANSparkMax::MotorType::kBrushless);
-    m_aimPid = new rev::CANPIDController(m_aimMotor->GetPIDController());
-    //Needs to be calibrated
-    m_aimPid->SetP(0.1); //6e-5
-    m_aimPid->SetI(0); //1e-6
-    m_aimPid->SetD(0);  //0.3
-    m_aimPid->SetIZone(0);  
-    m_aimPid->SetFF(0);  //000015
-    m_aimPid->SetOutputRange(-1.0, 1.0);
+  aiming_motor = new rev::CANSparkMax(aim_motor_id, rev::CANSparkMax::MotorType::kBrushless);
+  aiming_motor_pid_controller = new rev::CANPIDController(aiming_motor->GetPIDController());
+  //Needs to be calibrated
+  aiming_motor_pid_controller->SetP(0.1); //6e-5
+  aiming_motor_pid_controller->SetI(0); //1e-6
+  aiming_motor_pid_controller->SetD(0);  //0.3
+  aiming_motor_pid_controller->SetIZone(0);  
+  aiming_motor_pid_controller->SetFF(0);  //000015
+  aiming_motor_pid_controller->SetOutputRange(-1.0, 1.0);
 
-    upper_limit_switch = new frc::DigitalInput(1);
-    lower_limit_switch = new frc::DigitalInput(2);
+  upper_limit_switch = new frc::DigitalInput(1);
+  lower_limit_switch = new frc::DigitalInput(2);
 }
+
 Shooter::Shooter(rev::CANSparkMax *shooterMotor, rev::CANSparkMax *aimMotor) 
 {
-    m_shooterMotor = shooterMotor;
-    m_shooterPid = new rev::CANPIDController(m_shooterMotor->GetPIDController());
-    //Needs to be calibrated
-    m_shooterPid->SetP(6e-5);
-    m_shooterPid->SetI(1e-6);
-    m_shooterPid->SetD(0.3);
-    m_shooterPid->SetIZone(0);  
-    m_shooterPid->SetFF(0.000015);
-    m_shooterPid->SetOutputRange(-1.0, 1.0);
-    speed = 0.0; 
+  shooting_motor = shooterMotor;
+  shooting_motor_pid_controller = new rev::CANPIDController(shooting_motor->GetPIDController());
+  //Needs to be calibrated
+  shooting_motor_pid_controller->SetP(6e-5);
+  shooting_motor_pid_controller->SetI(1e-6);
+  shooting_motor_pid_controller->SetD(0.3);
+  shooting_motor_pid_controller->SetIZone(0);  
+  shooting_motor_pid_controller->SetFF(0.000015);
+  shooting_motor_pid_controller->SetOutputRange(-1.0, 1.0);
+  speed = 0.0; 
 
 
-    m_aimMotor = aimMotor;
-    m_aimPid = new rev::CANPIDController(m_aimMotor->GetPIDController());
-    //Needs to be calibrated
-    m_aimPid->SetP(0); //6e-5
-    m_aimPid->SetI(0); //1e-6
-    m_aimPid->SetD(0);  //0.3
-    m_aimPid->SetIZone(0);  
-    m_aimPid->SetFF(0);  //000015
-    m_aimPid->SetOutputRange(-1.0, 1.0);
- 
+  aiming_motor = aimMotor;
+  aiming_motor_pid_controller = new rev::CANPIDController(aiming_motor->GetPIDController());
+  //Needs to be calibrated
+  aiming_motor_pid_controller->SetP(0); //6e-5
+  aiming_motor_pid_controller->SetI(0); //1e-6
+  aiming_motor_pid_controller->SetD(0);  //0.3
+  aiming_motor_pid_controller->SetIZone(0);  
+  aiming_motor_pid_controller->SetFF(0);  //000015
+  aiming_motor_pid_controller->SetOutputRange(-1.0, 1.0);
 }
 
 void Shooter::Shoot(double shooterSpeed){
     speed = shooterSpeed;
-    m_shooterMotor->Set(speed);
+    shooting_motor->Set(speed);
 }
 void Shooter::AimHeight(double rotations) {
-    m_aimPid->SetReference(rotations, rev::ControlType::kPosition); 
+    aiming_motor_pid_controller->SetReference(rotations, rev::ControlType::kPosition); 
 }
 
 void Shooter::SetAimHeightP(double p){
-    m_aimPid->SetP(p);
+    aiming_motor_pid_controller->SetP(p);
 }
 
 void Shooter::SetShootingPercentOutput(double percent_output)
 {
-  m_shooterMotor->Set(percent_output);
+  shooting_motor->Set(percent_output);
 }
 
 void Shooter::SetAdjusterPercentOutput(double percent_output)
 {
+  // When the upper limit switch is reading true,
+  // we no longer want to go past the limit switch.
   if(upper_limit_switch->Get())
   {
     if(percent_output > 0.0)
@@ -78,6 +81,8 @@ void Shooter::SetAdjusterPercentOutput(double percent_output)
     }
   }
 
+  // When the lower limit switch is reading true,
+  // we no longer want to go past the limit switch.
   if(lower_limit_switch->Get())
   {
     if(percent_output < 0.0)
@@ -86,5 +91,7 @@ void Shooter::SetAdjusterPercentOutput(double percent_output)
     }
   }
   
-  m_aimMotor->Set(percent_output);
+  // After both safety checks, set the motor speed
+  // accordingly.
+  aiming_motor->Set(percent_output);
 }
